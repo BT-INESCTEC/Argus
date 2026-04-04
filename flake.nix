43d73f0c7fd76dd42b94f6de8b84b9374784521e
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2026-04-04 14:59:37 +0100

    added zizmor result saving

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Argus Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            poetry
+            zizmor
+          ];
+
+          shellHook = ''
+            echo "Argus dev shell"
+            echo "Go $(go version | awk '{print substr($3, 3)}')"
+            echo "Poetry $(poetry --version | awk -F '[ ()]' '{print $4}')"
+            echo "Zizmor $(zizmor --version | awk '{print $2}')"
+          '';
+        };
+      });
+}
